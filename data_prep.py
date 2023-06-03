13f2d98a107e16469bf28f2a77915860de964321
Liam McGlinchey <twoutrigger@outlook.com>
2023-06-03 18:08:30 -0400

    comment out embedding snippet

--- data_prep.py
+++ data_prep.py
@@ -22,7 +22,6 @@ def get_embedding(text, model="text-embedding-ada-002"):
    text = text.replace("\n", " ")
    return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
 
-df['ada_embedding'] = df.Text.apply(lambda x: get_embedding(x, model='text-embedding-ada-002'))
-
 # generate text embedding
+# df['ada_embedding'] = df.Text.apply(lambda x: get_embedding(x, model='text-embedding-ada-002'))
 # df.to_csv('text_embedding.csv', index=False)
