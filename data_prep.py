55a470f77425aaca60e2173f109f0e3b2f6ff44b
Liam McGlinchey <twoutrigger@outlook.com>
2023-06-03 18:04:35 -0400

    generate biblical text embedding

--- data_prep.py
+++ data_prep.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import openai
 
 orig = pd.read_csv("bible_american_standard_version.csv", header=0)
 
@@ -11,7 +12,17 @@ orig[['Book Name', 'Book Number', 'Chapter', 'Text']] = orig[['Book Name', 'Book
 # Aggregating biblical text to the chapter level
 df = orig.groupby(['Book Name', 'Book Number', 'Chapter'])['Text'].apply(' '.join).reset_index()
 
-print(len(df))
+df = df[['Text']]
 
-num_words = pd.Series(' '.join(df['Text']).split()).value_counts()
-print(len(num_words)) # roughly 30K tokens
+# estimating the number of tokens before generating embedding; roughly 30K tokens
+# num_words = pd.Series(' '.join(df['Text']).split()).value_counts()
+# print(len(num_words))
+
+def get_embedding(text, model="text-embedding-ada-002"):
+   text = text.replace("\n", " ")
+   return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+
+df['ada_embedding'] = df.Text.apply(lambda x: get_embedding(x, model='text-embedding-ada-002'))
+
+# generate text embedding
+# df.to_csv('text_embedding.csv', index=False)
